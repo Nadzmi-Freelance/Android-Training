3e2c1f83c743e60981c3b8c8760db8fa59866a4b
SeladangHijau <nadzmiidzham@gmail.com>
2017-11-15 21:18:12 +0800

    Settle bahagian registration

--- app/AndroidTutorial/app/src/main/java/com/seladanghijau/androidtutorial/Register.java
+++ app/AndroidTutorial/app/src/main/java/com/seladanghijau/androidtutorial/Register.java
@@ -1,19 +1,11 @@
 package com.seladanghijau.androidtutorial;
 
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -21,55 +13,37 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText etEmail, etPassword;
-    Button btnLogin, btnRegister;
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    EditText etEmail, etName, etPassword;
+    Button btnRegister;
 
     private RequestQueue requestQueue;
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor shEditor;
     private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
         // initialize views
         etEmail = (EditText) findViewById(R.id.etEmail);
+        etName = (EditText) findViewById(R.id.etName);
         etPassword = (EditText) findViewById(R.id.etPassword);
-        btnLogin = (Button) findViewById(R.id.btnLogin);
         btnRegister = (Button) findViewById(R.id.btnRegister);
 
-        // initialize OnClickListener
-        btnLogin.setOnClickListener(this);
+        // initialize listener
         btnRegister.setOnClickListener(this);
 
         // process
         requestQueue = Volley.newRequestQueue(this);
-        sharedPreferences = getApplication().getSharedPreferences("key", MODE_PRIVATE);
-
-        if(!sharedPreferences.getString("user-info", "").isEmpty()) {
-            startActivity(new Intent(this, Profile.class));
-            finish();
-        }
     }
 
     @Override
@@ -78,37 +52,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         progressDialog.setMessage("Please wait...");
         progressDialog.setCancelable(false);
 
-        switch(v.getId()) {
+        switch (v.getId()) {
             case R.id.btnRegister:
-                startActivity(new Intent(this, Register.class));
-                break;
-            case R.id.btnLogin:
-                final String email, password;
+                final String email, name, password;
                 String url;
 
-                url = "https://test-ground.000webhostapp.com/login.php";
+                url = "https://test-ground.000webhostapp.com/register.php";
 
                 email = etEmail.getText().toString();
+                name = etName.getText().toString();
                 password = etPassword.getText().toString();
 
                 try {
-                    StringRequest loginRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    StringRequest registerRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
                             try {
-                                if(!response.isEmpty()) {
-                                    shEditor = sharedPreferences.edit();
+                                JSONObject jsonResponse = new JSONObject(response);
 
-                                    shEditor.putString("user-info", response);
-                                    shEditor.commit();
-
-                                    Intent profile = new Intent(getApplicationContext(), Profile.class);
-                                    profile.putExtra("info", response);
-
-                                    startActivity(profile);
+                                if(jsonResponse.getString("status").equalsIgnoreCase("1")) {
+                                    Toast.makeText(getApplicationContext(), "Registration successful.", Toast.LENGTH_LONG).show();
                                     finish();
                                 } else
-                                    Toast.makeText(getApplicationContext(), "No record found", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(getApplicationContext(), "Failed to register.", Toast.LENGTH_LONG).show();
                             } catch (Exception e) { e.printStackTrace(); }
 
                             if(progressDialog.isShowing())
@@ -117,25 +83,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getApplicationContext(), "Error occured", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "An error has occurred.", Toast.LENGTH_LONG).show();
 
                             if(progressDialog.isShowing())
                                 progressDialog.dismiss();
                         }
-                    })
-                    {
+                    }){
                         @Override
                         protected Map<String, String> getParams() throws AuthFailureError {
-                            Map<String, String> params = new HashMap<>();
+                            Map<String, String> params;
 
+                            params = new HashMap<>();
                             params.put("email", email);
+                            params.put("name", name);
                             params.put("password", password);
 
                             return params;
                         }
                     };
 
-                    requestQueue.add(loginRequest);
+                    requestQueue.add(registerRequest);
                     progressDialog.show();
                 } catch (Exception e) { e.printStackTrace(); }
                 break;
